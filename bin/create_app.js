0d038d452f6d96a577fe1b91cbe612f45ad621f5
Nhan Nguyen <nhan.nguyen@katalon.com>
2024-06-19 21:36:12 +0700

    use my project

--- bin/create_app.js
+++ bin/create_app.js
@@ -13,7 +13,7 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 const projectPath = path.join(currentPath, projectName);
-const GIT_REPO = 'https://github.com/katalon-studio/katalon-webapp-template.git';
+const GIT_REPO = 'https://github.com/nguyen-tri-nhan/sample_template.git';
 
 if (projectName !== '.') {
   try {
